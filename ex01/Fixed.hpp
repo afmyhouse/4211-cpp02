ace3dc6e8fa548fd140291d6ada39531161e7346
afmyhouse <af.myhouse@gmail.com>
2024-09-25 10:38:23 +0100

    test snnipets

--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: antoda-s <antoda-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 10:00:26 by antoda-s          #+#    #+#             */
-/*   Updated: 2024/05/07 10:01:31 by antoda-s         ###   ########.fr       */
+/*   Updated: 2024/09/25 10:38:17 by antoda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ class Fixed
 	private:
 		int fixedPointNumber;
 		static const int fractionalSize = 8;
-		
+
 	public:
 		//! Constructors and destructors
 		// Default constructor
@@ -30,13 +30,13 @@ class Fixed
 
 		// Copy constructor
 		Fixed(const Fixed &fixedPointObj);
-		
+
 		// Int constructor
 		Fixed(const int pureIntNum);
 
 		// Float constructor
 		Fixed(const float pureFloatNum);
-		
+
 		//! Copy assignment
 		Fixed& operator=(const Fixed& fixedPointObj);
 
@@ -46,7 +46,7 @@ class Fixed
 		float toFloat(void) const;
 		int toInt(void) const;
 
-		//! Getters and setters 
+		//! Getters and setters
 		int getRawBits(void) const;
 		void setRawBits(int const pureIntNum);
 
---
+++ ex01/test.cpp
@@ -0,0 +1,15 @@
+#include "fixed.hpp"
+
+fixed :: fixed() {}
+
+// fixed :: fixed(const fixed& srcClass) {
+// 	*this = srcClass;
+// }
+
+// fixed& fixed :: operator=(const fixed& srcClass) {
+// 	if (this != &srcClass)
+// 		*this = srcClass;
+// 	return (*this);
+// }
+
+fixed :: ~fixed() {}
